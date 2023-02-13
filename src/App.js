80b1542c5dafc8f2b4d61988e936db7c8c1e76b4
joonaskopsala <joonas.kopsala@gmail.com>
2023-02-13 15:38:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Button, Box, CircularProgress, Typography} from '@mui/material';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GuessImage = () => {
+  const [image, setImage] = useState(null);
+  const [guess, setGuess] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default App;
+  const handleFileInputChange = (event) => {
+    setImage(event.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append('image', image);
+
+    try {
+      const response = await axios.post('http://localhost:8080/guess', formData);
+      setGuess(response.data.guess);
+    } catch (error) {
+      console.error(error);
+      setGuess('An error occurred');
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Box className="root">
+      <form className="form">
+        <input
+          accept="image/*"
+          className="fileInput"
+          id="image-input"
+          type="file"
+          onChange={handleFileInputChange}
+        />
+        <label htmlFor="image-input" className="fileLabel">
+          <Button variant="contained" component="span">
+            Select Image
+          </Button>
+        </label>
+        <Button
+          className="submitButton"
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          disabled={!image}
+        >
+          Submit
+        </Button>
+      </form>
+      {loading ? (
+        <CircularProgress className="progress" />
+      ) : (
+        guess && (
+          <Typography variant="body1" className="guess">
+            The image contains: {guess}
+          </Typography>
+        )
+      )}
+    </Box>
+  );
+};
+
+export default GuessImage;
